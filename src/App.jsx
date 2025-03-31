842e79bdef14628e6ed0f808b61f16ce7a26bec3
Offixialtrust <richardtrust12@gmail.com>
2025-03-31 23:16:12 +0100

    comnit

--- src/App.jsx
+++ src/App.jsx
@@ -139,7 +139,7 @@ function App() {
               <div key={country.name} className="individual-country mode">
                 <img src={country.flags.png} alt={`${country.name}'s flag`} />
                 <div className="country-info">
-                  <p className="large-text">{country.name}</p>
+                  <h2 className="large-text">{country.name}</h2>
                   <p className="small-bold-text">Population: <span className="small-text">{(country.population).toLocaleString()}</span></p>
                   <p className="small-bold-text">Region: <span className="small-text">{country.region}</span></p>
                   <p className="small-bold-text">Capital: <span className="small-text">{country.capital}</span></p>
